f8f6cef63425d48eccf3c927600145e96f4a685e
Hossein <mhusseinkarimi@gmail.com>
2022-02-03 10:42:20 +0330

    Add information_registration.py to src
    This script implements methods to read and write information from and to database.

---
+++ src/information_registration.py
@@ -0,0 +1,20 @@
+"Information regitration module"
+
+def read_from_DB():
+    """Reads information from database asserted that the user has access.
+    """
+    pass
+
+def write_to_DB():
+    """Writes information to database (Only admin can use this method).
+    """
+    pass
+
+def get_info():
+    """Submits the formatted output information that is requested.
+    """
+    pass
+
+def import_info():
+    """Writes the formatted input information that is provided by user (Admin should provide access)."""
+    pass
