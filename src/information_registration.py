3c1562549959c9798421dbc3da7f7477dbc46706
Kimia_ghodoosi <kimiaghodoosi@gmail.com>
2022-02-07 13:48:25 +0330

    read from DB is added

--- src/information_registration.py
+++ src/information_registration.py
@@ -1,9 +1,16 @@
 "Information regitration module"
+import pandas as pd
 
-def read_from_DB():
+def read_from_DB(file):
     """Reads information from database asserted that the user has access.
     """
-    pass
+    dataset = pd.read_csv(file, encoding='utf-8')
+    for index, row in dataset.iterrows():
+        first_name = row[0]
+        last_name = row[1]
+        personal_id = row[2]
+        date_of_birth = pd.to_datetime(row[3]).strftime('%m/%d/%Y')
+        return first_name, last_name, personal_id, date_of_birth
 
 def write_to_DB():
     """Writes information to database (Only admin can use this method).
