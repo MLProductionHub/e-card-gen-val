20a693b78addddc3956f219dbcb412c48af98d4e
Hossein <mhusseinkarimi@gmail.com>
2022-02-08 17:12:55 +0330

    Add src/registration/information_registration.py
    This module provides the functionlaities to add information to a database as a registration function.

---
+++ src/registeration/information_registration.py
@@ -0,0 +1,31 @@
+"Information regitration module"
+from pathlib import Path
+from typing import Union
+import pandas as pd
+
+
+def read_from_DB(file: Union[str, Path]):
+    """Reads information from database asserted that the user has access.
+    """
+    dataset = pd.read_csv(file, encoding='utf-8')
+    for index, row in dataset.iterrows():
+        first_nam = row[0]
+        last_name= row[1]
+        personal_id = row[2]
+        date_of_birth = pd.to_datetime(row[3]).strftime('%m/%d/%Y')
+        return first_nam, last_name, personal_id, date_of_birth
+    pass
+
+def write_to_DB():
+    """Writes information to database (Only admin can use this method).
+    """
+    pass
+
+def get_info():
+    """Submits the formatted output information that is requested.
+    """
+    pass
+
+def import_info():
+    """Writes the formatted input information that is provided by user (Admin should provide access)."""
+    pass
