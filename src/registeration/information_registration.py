22eeda3bf284db7bd29c0738ce91d47aa8c49640
Kimia_ghodoosi <kimiaghodoosi@gmail.com>
2022-02-08 22:21:40 +0330

    reading from database is added

--- src/registeration/information_registration.py
+++ src/registeration/information_registration.py
@@ -1,32 +1,32 @@
 "Information regitration module"
 from pathlib import Path
 from typing import Union
-import pandas as pd
 from csv import writer
-
+import csv
 
 def read_from_DB(file: Union[str, Path]):
     """Reads information from database asserted that the user has access.
     """
-    dataset = pd.read_csv(file, encoding='utf-8')
-    for index, row in dataset.iterrows():
-        first_nam = row[0]
-        last_name = row[1]
-        personal_id = row[2]
-        date_of_birth = pd.to_datetime(row[3]).strftime('%m/%d/%Y')
-        return first_nam, last_name, personal_id, date_of_birth
-    pass
+    with open(file) as file_obj:
+        # Create reader object by passing the file
+        # object to DictReader method
+        reader_obj = csv.DictReader(file_obj)
+
+        # Iterate over each row in the csv file
+        # using reader object
+        for row in reader_obj:
+            print(row)
 
 
 def write_to_DB(db_file, first_name, last_name, personal_id, date_of_birth):
     """
 
     Args:
-        db_file:
-        first_name:
-        last_name:
-        personal_id:
-        date_of_birth:
+        db_file: the database file
+        first_name: the first name of a person
+        last_name: the last name of a person
+        personal_id: the personal id of a person
+        date_of_birth: the date of birth of a person
 
     Returns: Writes information to database (Only admin can use this method)
 
@@ -55,5 +55,5 @@ def import_info():
     """Writes the formatted input information that is provided by user (Admin should provide access)."""
     pass
 
-
+# print(read_from_DB("idcard.csv"))
 # write_to_DB("idcard.csv", "alireza", "ahmadi", 4, '03-06-1997')
