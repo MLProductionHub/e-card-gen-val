8fbb731e8006586a1ced378bdeaac086e9c8850f
Hossein <mhusseinkarimi@gmail.com>
2022-02-17 11:41:11 +0330

    Add write_to_database method to src/database/database.py
    This method takes the iformation that is going to be added to the database in form of a dictionary
    and adds them to the database if exists and if not initiates the database.

--- src/database/database.py
+++ src/database/database.py
@@ -1,6 +1,6 @@
 from genericpath import exists
 from pathlib import Path
-from typing import List, Union
+from typing import Dict, List, Union
 import os
 import csv
 
@@ -24,6 +24,42 @@ def init_database(database_path: Union[str, Path], headers: List[str]) -> str:
             writer.writerow(headers) # write the header line
     
     return str(database_path) # print the file address
-    
+
+def write_to_database(database: Union[str, Path], info: Dict[str, object]):
+    """Writes a row in the dataset given the values as a dictionary.
+
+    Args:
+        database (Union[str, Path]): The path to the database we want to use or initiate
+        info (Dict[str, object]): The dictionary contatining the key and values. (Values should be iterables)
+    """
+    database = os.path.expanduser(database) # create the absolut path to database
+    if os.path.exists(database):
+        # the case that database already exists
+        args = list(info.values()) # values to be stored
+        try: 
+            for values in args:
+                assert len(values) == len(args[0]) # checking that all lists have the same length
+            args_size = len(args[0])
+        
+        except AssertionError:
+            print("The values are not the same size.")
+            args_size = min([len(values) for values in args]) # in case of different length the minimum length is considered
 
 
+        with open(database, "a") as file: # writing in database
+            writer = csv.writer(file) 
+            for i in range(args_size):
+                row  = []
+                for values in args:
+                    row.append(values[i])
+                writer.writerow(row)
+    else: # case of database not existing
+        headers = list(info.keys()) # headers of database
+        database_path = init_database(database, headers) # initializing the database
+        with open(database_path, "a") as file: # writing in the database
+            writer = csv.writer(file)
+            for i in range(args_size):
+                row  = []
+                for values in args:
+                    row.append(values[i])
+                writer.writerow(row)
